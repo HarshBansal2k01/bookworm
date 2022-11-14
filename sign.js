9e7125722b8597644de2f702d2473b2e3efa360c
HarshBansal2k01 <ihbbro14@gmail.com>
2022-11-14 11:50:26 +0530

    update

--- sign.js
+++ sign.js
@@ -1,11 +1,22 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
+
+function go() {
+  var email = document.myform.email.value;
+  var password = document.myform.password.value;
+
+  if (email == "" || password == "") {
+    alert("Enter Email and Password");
+    return false;
+  }
+
+}
